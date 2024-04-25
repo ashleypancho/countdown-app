ccc11057b6e8ad1cf2e90a9e2837f0ceb2b891de
Ashley Pancho <apancho@smu.edu>
2024-04-25 01:33:04 -0500

    implemented letter selection and word validation against selected letters

--- countdown/src/app/letters/letters.component.ts
+++ countdown/src/app/letters/letters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import letterFrequencies from './letter-frequency.json';
 
 @Component({
   selector: 'app-letters',
@@ -7,26 +8,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LettersComponent implements OnInit {
   isToastOpen: boolean = false;
-  phase: string = 'entry';
+  phase: string = 'letterSelection';
   errorMessage: string = '';
   wordlist: string[] = [];
 
+  vowels: string[] = [];
+  consonants: string[] = [];
+  vowelCount: number = 0;
+  consonantCount: number = 0;
+  vowelButtonDisabled: boolean = false;
+  consonantButtonDisabled: boolean = false;
+
+  lettersList: string[] = [];
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    for (const vowel in letterFrequencies.vowels) {
+      for (let i = 0; i < (letterFrequencies.vowels as any)[vowel]; i++) {
+        this.vowels.push(vowel);
+      }
+    }
+
+    for (const consonants in letterFrequencies.consonants) {
+      for (let i = 0; i < (letterFrequencies.consonants as any)[consonants]; i++) {
+        this.consonants.push(consonants);
+      }
+    }
+    this.vowels.sort((a, b) => 0.5 - Math.random());
+    this.consonants.sort((a, b) => 0.5 - Math.random());
+  }
+
+  selectConsonant() {
+    if (this.consonants.length) {
+      const selectedIdx = Math.floor(Math.random() * this.consonants.length);
+      this.lettersList.push(this.consonants[selectedIdx]);
+      this.consonants.splice(selectedIdx, 1);
+      this.consonantCount++;
+      
+      // check if we have enough consonants
+      if (this.consonantCount >= 6) {
+        // we don't really need to check > but just in case
+        this.consonantButtonDisabled = true;
+      }
+      this.checkSelectionComplete();
+    }
+  }
+
+  selectVowel() {
+    if (this.vowels.length) {
+      const selectedIdx = Math.floor(Math.random() * this.vowels.length);
+      this.lettersList.push(this.vowels[selectedIdx]);
+      this.vowels.splice(selectedIdx, 1);
+      this.vowelCount++;
+
+      // check if we have enough vowels
+      if (this.vowelCount >= 5) {
+        // we don't really need to check > but just in case
+        this.vowelButtonDisabled = true;
+      }
+      this.checkSelectionComplete();
+    }
+  }
+
+  checkSelectionComplete() {
+    if (this.lettersList.length === 9) {
+      this.phase = "entry";
+    }
+  }
 
   submitWord() {
     const input = (document.getElementById('word') as HTMLInputElement);
+    const wordFoundInLettersList = this.isValidWord(input.value.toUpperCase());
     if (this.hasNumber(input.value)) {
       this.errorMessage = 'Invalid word: Word may not contain numbers';
       this.setOpen(true);
     } else if (this.wordlist.includes(input.value)) {
       this.errorMessage = "Word already submitted";
       this.setOpen(true);
+    } else if (!wordFoundInLettersList) {
+      this.errorMessage = "Invalid word: Word not valid based on given letters";
+      this.setOpen(true);
     } else if (input.value.length > 0 && !this.wordlist.includes(input.value)) {
       this.wordlist.push(input.value);
     }
     (document.getElementById('word') as HTMLInputElement).value = '';
+  }
+
+  isValidWord(word: string) {
+    const validLetters = this.lettersList.slice();
+    for (let i = 0; i < word.length; i++) {
+      if (validLetters.includes(word.charAt(i))) {
+        // check if validLetters array contains the letter of the word
+        // if it is present in the validLetters array, remove it
+        validLetters.splice(validLetters.indexOf(word.charAt(i)), 1);
+      } else {
+        // the given word cannot be made from the list of letters, return false
+        return false;
+      }
+    }
+
+    // for loop exited, which means word must be valid
+    return true;
   }
 
   setOpen(isOpen: boolean) {
